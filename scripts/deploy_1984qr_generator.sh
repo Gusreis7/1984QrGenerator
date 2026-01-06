5e5b07afc62c16faa6a90deba82453a87ccda152
Gustavo <gustavo@debian>
2026-01-06 18:54:35 -0300

    update run job variavel

--- scripts/deploy_1984qr_generator.sh
+++ scripts/deploy_1984qr_generator.sh
@@ -2,11 +2,9 @@
 set -e
 
 BASE_DIR="$PWD"
-$dir = "flask_app"
+dir="flask_app"  # <-- sem $ e sem espaço
 echo "Atualizando serviço: $BASE_DIR/$dir"
 cd "$BASE_DIR/$dir"
 
 docker compose down
 docker compose up -d
-
- 
